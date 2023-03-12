06f102c29038867a8794d10334d191d3b300aa47
Rikuto <orikutobasuke@gmail.com>
2023-03-12 23:40:44 +0900

    gurappu2 add

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import {Box,Flex,Center} from '@chakra-ui/react'
+
+export default function HeaderComonents(){
+    return(
+        <>
+            <Box w={'100%'}>
+                <Flex w={'100%'} justifyContent={'flex-end'}>
+                    <Box w={'10em'} h={'10em'} bgColor={'#ff9d9d'}>
+
+                    </Box>
+                </Flex>
+            </Box>
+        </>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+import {Box,Text,Flex,Center} from '@chakra-ui/react'
+import Header from '@/components/Header'
+
+export default function Index(){
+  return(
+    <>
+      <Box>
+        <Header/>
+      </Box>
+    </>
+  )
+}
