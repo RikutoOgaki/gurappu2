8c8758dcd159432318af981b6b48df0c2ba0e721
Rikuto <orikutobasuke@gmail.com>
2023-03-17 09:26:23 +0900

    gurappu2 add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,12 @@ import { Box, Flex, Center } from '@chakra-ui/react'
 export default function HeaderComonents() {
     return (
         <>
-            <Box w={'100%'}>
+            <Box as='header' w={'100%'}>
                 <Flex w={'100%'} justifyContent={'flex-end'}>
                     <Box w={'10em'} h={'10em'} bgColor={'#ff9d9d'}>
-
+                        <Box as='span'></Box>
+                        <Box as='span'></Box>
+                        <Box as='span'></Box>
                     </Box>
                 </Flex>
             </Box>
