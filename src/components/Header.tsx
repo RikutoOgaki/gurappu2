3a7acb03e35a8dee15bfc5cf97a1d94fb6277e76
Rikuto <orikutobasuke@gmail.com>
2023-03-23 07:25:34 +0900

    gurappu2 add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ export default function HeaderComonents() {
         <>
             <Box as='header' w={'100%'}>
                 <Flex w={'100%'} justifyContent={'flex-end'}>
-                    <Box w={'10em'} h={'10em'} bgColor={'#ff9d9d'}>
-                        <Box as='span'></Box>
-                        <Box as='span'></Box>
-                        <Box as='span'></Box>
-                    </Box>
+                    <Flex flexDir={'column'} gap={'1.5rem'} w={'10em'} h={'10em'} bgColor={'#ff9d9d'}>
+                        <Box as='span' border={'2px solid #fff'} margin={'2rem auto 0'} w={'80%'}></Box>
+                        <Box as='span' border={'2px solid #fff'} margin={'1rem auto 0'} w={'80%'}></Box>
+                        <Box as='span' border={'2px solid #fff'} margin={'1rem auto 0'} w={'80%'}></Box>
+                    </Flex>
                 </Flex>
             </Box>
         </>
